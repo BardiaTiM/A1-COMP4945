3df1d16cf02b7657aa0d51731fad4a9fb7e6cb23
Bardia <bilbeigitimouri@my.bcit.ca>
2024-02-01 10:36:12 -0800

    Added threadpool.

--- Assignment1/UploadServer.cs
+++ Assignment1/UploadServer.cs
@@ -20,20 +20,27 @@ namespace Assignment1 {
 				return;
 			}
 
+			//Set the number of threads based on CPU Cores
+			int maxThreads = Environment.ProcessorCount;
+			ThreadPool.SetMaxThreads(maxThreads, maxThreads);
+
 			while (true) {
 				try {
 					TcpClient client = server.AcceptTcpClient();
 					Console.WriteLine("Client connected.");
-
-					// Extract the socket from the TcpClient
-					Socket clientSocket = client.Client;
-
-					// Create a new instance of UploadServerThread with the socket
-					UploadServerThread serverThread = new UploadServerThread(clientSocket);
-
-					// Start the thread
-					Thread clientThread = new Thread(serverThread.Run);
-					clientThread.Start();
+					
+					//Using ThreadPool to manage the client handling
+					ThreadPool.QueueUserWorkItem(state =>
+					{
+						// Extract the socket from the TcpClient
+						Socket clientSocket = client.Client;
+						
+						// Create a new instance of UploadServerThread with the socket
+						UploadServerThread serverThread = new UploadServerThread(clientSocket);
+						
+						// Start the thread
+						serverThread.Run();
+					});
 				}
 				catch (Exception e) {
 					Console.WriteLine("Error occurred: " + e.Message);
